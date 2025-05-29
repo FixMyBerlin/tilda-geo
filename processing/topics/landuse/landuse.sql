fc34fcbe5a96a9c248e91e2e02d5c520b74eb50c
Tobias <tobias@fixmycity.de>
2025-05-29 21:05:05 +0200

    Processing: Experimental landuse_settlements, roads in settlements, bikelanes outside settlements
    This is to test out the approach of adding data data via landuse of settlements and pseudo tags. The idea is to use those roads, bikelanes data as pseudo tags for the next processing run.
    
    The landuse_settlements takes 3,5 Min for Berlin-Brandenburg region.

---
+++ processing/topics/landuse/landuse.sql
@@ -0,0 +1,4 @@
+
+\i '/processing/topics/landuse/landuse_settlements.sql'
+\i '/processing/topics/landuse/roads_in_settlements.sql'
+\i '/processing/topics/landuse/bikelanes_outside_settlements.sql'
