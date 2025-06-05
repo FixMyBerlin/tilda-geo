b828883c525b3dc5ef6e0f95943fbcc698e42c54
Laurenz Rasche <laurenz@fixmycity.de>
2025-06-05 13:10:57 +0200

    improve capacity estimation by projecting the padding

--- processing/topics/parking/custom_functions/estimate_capacity.sql
+++ processing/topics/parking/custom_functions/estimate_capacity.sql
@@ -1,20 +1,24 @@
 CREATE OR REPLACE FUNCTION estimate_capacity (length NUMERIC, orientation TEXT) RETURNS INTEGER AS $$
 DECLARE
-  car_length NUMERIC := 5;
-  car_width NUMERIC := 2.5;
-  parking_angle NUMERIC := Radians(60); -- 0 degrees = parallel
+  car_length NUMERIC := 4.4;
+  car_width NUMERIC := 2;
+  parking_angle NUMERIC := Radians(60); -- 0 degrees = perpendicular
   space_per_car NUMERIC;
+  padding_parallel NUMERIC := 0.8;
+  padding_prependicular NUMERIC := 0.5;
   padding NUMERIC;
   n_cars INTEGER;
 BEGIN
 
   IF orientation = 'parallel' OR orientation IS NULL THEN
     space_per_car := car_length;
+    padding := padding_parallel;
   ELSIF orientation = 'diagonal' THEN
-    space_per_car := Cos(parking_angle) * car_length + Sin(parking_angle) * car_width;
-    -- TODO maybe also transfrom the padding to diagonal?
+    space_per_car :=  Sin(parking_angle) * car_length + Cos(parking_angle) * car_width;
+    padding := Cos(parking_angle) * padding_prependicular;
   ELSIF orientation = 'perpendicular' THEN
     space_per_car := car_width;
+    padding := padding_prependicular;
   ELSE
     RAISE EXCEPTION 'Invalid orientation: %, must be "parallel" or "diagonal"', orientation;
   END IF;
