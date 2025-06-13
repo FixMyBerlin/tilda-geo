a2281371f499232383488126fa3c35e7fa8fe874
Tobias <tobias@fixmycity.de>
2025-06-13 07:07:55 +0200

    Processing: Remove production behavior from sanitize_for_logging
    The code did not behave the same for both ENV and it is not worth debugging this; we can just create the error tables as well.

--- processing/topics/helper/__tests__/sanitize_for_logging.test.lua
+++ processing/topics/helper/__tests__/sanitize_for_logging.test.lua
@@ -1,50 +1,24 @@
 require('init')
-require("sanitize_for_logging")
-local ENV = require("ENV")
+require('sanitize_for_logging')
 
-describe("sanitize_for_logging on staging, development", function()
-  -- Overwrite what is used in sanitize_for_logging
-  -- But also this is nil for testing because env variables do not work in testing.
-  ENV.is_production = false
-
-  it("returns value if allowed", function()
-    local value = sanitize_for_logging("value_foo", {"value_foo"})
-    assert.are.equal(value, "value_foo")
+describe('sanitize_for_logging', function()
+  it('returns value if allowed', function()
+    local value = sanitize_for_logging('value_foo', {'value_foo'})
+    assert.are.equal(value, 'value_foo')
   end)
 
-  it("return nil if value nil", function()
-    local value = sanitize_for_logging(nil, {"value_foo"})
+  it('return nil if value nil', function()
+    local value = sanitize_for_logging(nil, {'value_foo'})
     assert.are.equal(value, nil)
   end)
 
-  it("return DISALLOWED_VALUE if value not allowed", function()
-    local value = sanitize_for_logging("value_not_allowed", {"value_foo"})
-    assert.are.equal(value, "DISALLOWED_VALUE")
+  it('return DISALLOWED_VALUE if value not allowed', function()
+    local value = sanitize_for_logging('value_not_allowed', {'value_foo'})
+    assert.are.equal(value, 'DISALLOWED_VALUE')
   end)
 
-  it("return nil if value not ignored", function()
-    local value = sanitize_for_logging("yes", {"no"}, {"yes"})
-    assert.are.equal(value, nil)
-  end)
-end)
-
-describe("sanitize_for_logging on production", function()
-  -- Overwrite what is used in sanitize_for_logging
-  -- But also this is nil for testing because env variables do not work in testing.
-  ENV.is_production = true
-
-  it("returns value if allowed", function()
-    local value = sanitize_for_logging("value_foo", {"value_foo"})
-    assert.are.equal(value, "value_foo")
-  end)
-
-  it("return nil if value nil", function()
-    local value = sanitize_for_logging(nil, {"value_foo"})
-    assert.are.equal(value, nil)
-  end)
-
-  it("return nil if value not allowed", function()
-    local value = sanitize_for_logging("value_not_allowed", {"value_foo"})
+  it('return nil if value not ignored', function()
+    local value = sanitize_for_logging('yes', {'no'}, {'yes'})
     assert.are.equal(value, nil)
   end)
 end)
