bea1f52bf2627adfc28c1d46c82cf1816fc0ef7e
Tobias <tobias@fixmycity.de>
2025-06-25 21:54:13 +0200

    Processing: Use sanitize helper to cleanup surface values

--- processing/topics/helper/DeriveSurface.lua
+++ processing/topics/helper/DeriveSurface.lua
@@ -1,8 +1,15 @@
-local function surfaceDirect(surface)
-  if surface ~= nil then
+local SANITIZE_TAGS = require('sanitize_tags')
+local SANITIZE_VALUES = require('sanitize_values')
+
+local function surfaceDirect(tags)
+  if tags.surface ~= nil then
     local source = "tag"
     local confidence = "high"
 
+    -- We use the same sanitizer but we dont have a setup to handle the disallowed values, yet, so we remove them right away.
+    -- In the future, we want to setup a system similar to _parking_errors.
+    local surface = SANITIZE_TAGS.surface(tags)
+    if surface == SANITIZE_VALUES.disallowed then return nil, nil, nil end
 
     return surface, source, confidence
   end
@@ -11,6 +18,6 @@ local function surfaceDirect(surface)
 end
 
 function DeriveSurface(tags)
-  local surface, source, confidence = surfaceDirect(tags.surface)
+  local surface, source, confidence = surfaceDirect(tags)
   return { surface = surface, surface_source = source, surface_confidence = confidence }
 end
