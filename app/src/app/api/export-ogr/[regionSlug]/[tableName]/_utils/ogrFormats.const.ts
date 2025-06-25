a55fa82791bf39cab5f2bc918c8f248c59d82904
Tobias <tobias@fixmycity.de>
2025-06-25 16:48:15 +0200

    Export: Disable parquet again
    Revert 231700b8e075edba44f954015f89794f5eab1de1

--- app/src/app/api/export-ogr/[regionSlug]/[tableName]/_utils/ogrFormats.const.ts
+++ app/src/app/api/export-ogr/[regionSlug]/[tableName]/_utils/ogrFormats.const.ts
@@ -1,8 +1,15 @@
-export const formats = ['geojson', 'gpkg', 'fgb', 'geoparket'] as const
+export const formats = [
+  'geojson',
+  'gpkg',
+  'fgb',
+  // geoparket requires gdal 3.9+ which is not available in node:22-bookworm-slim (app.Dockerfile)
+  // (see https://gdal.org/en/stable/drivers/vector/parquet.html)
+  // 'geoparket'
+] as const
 
 export const ogrFormats: Record<(typeof formats)[number], string> = {
   geojson: 'GeoJSON',
   gpkg: 'GPKG',
   fgb: 'FlatGeobuf',
-  geoparket: 'Parquet',
+  // geoparket: 'Parquet',
 } as const
