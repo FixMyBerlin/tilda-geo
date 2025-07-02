74f6ab8f8b51b955a1a56062edad0969539e8310
Tobias <tobias@fixmycity.de>
2025-07-02 12:15:53 +0200

    Processing: Cleanup oneway processing for roads

--- processing/topics/roads_bikelanes/roads/RoadClassification.lua
+++ processing/topics/roads_bikelanes/roads/RoadClassification.lua
@@ -27,23 +27,21 @@ function RoadClassification(object)
   --   end
   -- end
 
-
   if tags.oneway == 'yes' then
     -- Note: We do not pass 'oneway=no' to the 'road_oneway' key
     -- because it is the default which we do not want to show in the UI.
-    result_tags.road_oneway = tags.oneway
-    if tags.dual_carriageway == "yes" then
-      result_tags.road_oneway = tags.oneway .. '_dual_carriageway'
+    result_tags.road_oneway = 'yes'
+    if tags.dual_carriageway == 'yes' then
+      result_tags.road_oneway = 'yes_dual_carriageway'
     end
   end
-  if tags['oneway:bicycle'] == 'no' or tags['oneway:bicycle'] then
-    result_tags['road_oneway:bicycle'] = tags['oneway:bicycle']
+  if tags['oneway:bicycle'] then
+    result_tags['road_oneway:bicycle'] = Sanitize(tags['oneway:bicycle'], { 'yes', 'no' })
   end
 
   CopyTags(result_tags, tags, tags_copied)
   CopyTags(result_tags, tags, tags_prefixed, "osm_")
   result_tags.width = parse_length(tags.width)
-  result_tags.oneway = Sanitize(tags.oneway, { "yes", "no" })
   result_tags.bridge = Sanitize(tags.bridge, { "yes" })
   result_tags.tunnel = Sanitize(tags.tunnel, { "yes" })
   MergeTable(result_tags, DeriveTrafficSigns(tags))
