9074a0f554215a0d5b83d4d8695875d9ae525765
Laurenz Rasche <laurenz@fixmycity.de>
2025-07-04 11:52:39 +0200

    add `_parking_discarded` for parkings that don't fullfill the requirments

--- processing/topics/parking/5_estimate_parking_capacities.sql
+++ processing/topics/parking/5_estimate_parking_capacities.sql
@@ -22,15 +22,40 @@ SET
 WHERE
   tags ->> 'capacity' IS NULL;
 
-DELETE FROM _parking_parkings_merged
+DROP TABLE IF EXISTS _parking_discarded;
+
+SELECT
+  * INTO _parking_discarded
+FROM
+  _parking_parkings_merged
 WHERE
   (tags ->> 'capacity')::INTEGER < 1;
 
+UPDATE _parking_discarded
+SET
+  tags = tags || '{"reason": "capacity < 1"}'::JSONB;
+
+CREATE INDEX parking_discarded_idx ON _parking_discarded USING BTREE (id);
+
+DELETE FROM _parking_parkings_merged
+WHERE
+  id IN (
+    SELECT
+      id
+    FROM
+      _parking_discarded
+  );
+
 -- MISC
 ALTER TABLE _parking_parkings_merged
+ALTER COLUMN geom TYPE geometry (Geometry, 5243) USING ST_SetSRID (geom, 5243);
+
+ALTER TABLE _parking_discarded
 ALTER COLUMN geom TYPE geometry (Geometry, 5243) USING ST_SetSRID (geom, 5243);
 
 -- create an index on the merged table
 CREATE INDEX parking_parkings_merged_geom_idx ON _parking_parkings_merged USING GIST (geom);
 
 CREATE INDEX parking_parkings_merged_osm_ids_idx ON _parking_parkings_merged USING GIN (original_osm_ids);
+
+CREATE INDEX parking_parkings_discared_geom_idx ON _parking_discarded USING GIST (geom);
