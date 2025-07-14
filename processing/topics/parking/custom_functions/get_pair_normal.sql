9d584cc93919722065fc54a132988312efff7d16
Laurenz Rasche <laurenz@fixmycity.de>
2025-07-14 12:49:31 +0200

    offset normal slightly to avoid intersection with origin

--- processing/topics/parking/custom_functions/get_pair_normal.sql
+++ processing/topics/parking/custom_functions/get_pair_normal.sql
@@ -2,15 +2,21 @@ DROP FUNCTION IF EXISTS get_pair_normal;
 
 CREATE FUNCTION get_pair_normal (a geometry, b geometry, len float) RETURNS geometry AS $$
 DECLARE
-  normal_azimuth double precision := ST_Azimuth(a, b) + pi() / 2;
-  midpoint geometry := ST_Centroid(ST_Union(a, b));
+  normal_azimuth double precision ;
+  midpoint geometry;
+  startpoint geometry;
   endpoint geometry;
 BEGIN
 
-  -- Create endpoint of the normal vector using length and angle
+  -- get the normals azimuth
+  normal_azimuth := ST_Azimuth(a, b) + pi() / 2;
+  midpoint  := ST_Centroid(ST_Union(a, b));
+
+  -- Create start and end point of the normal vector using length and angle
+  startpoint := ST_Project(midpoint, 0.1, normal_azimuth);
   endpoint := ST_Project(midpoint, len, normal_azimuth);
 
   -- Return line from midpoint to normal endpoint
-  RETURN ST_MakeLine(midpoint, endpoint);
+  RETURN ST_MakeLine(startpoint, endpoint);
 END;
 $$ LANGUAGE plpgsql IMMUTABLE STRICT;
