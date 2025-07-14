fd17302521d1590e08de0e6515bdd0b99b993268
Tobias <tobias@fixmycity.de>
2025-07-14 15:41:01 +0200

    Processing: Refactor tests
    - left is the input value "passed in"
    - right is the processed value "expected"

--- processing/topics/parking/roads/__tests__/result_tags_roads.test.lua
+++ processing/topics/parking/roads/__tests__/result_tags_roads.test.lua
@@ -1,41 +1,41 @@
-describe("`result_tags_roads`", function()
+describe('`result_tags_roads`', function()
   require('init')
-  require("result_tags_roads")
-  require("Log")
+  require('result_tags_roads')
+  require('Log')
   require('osm2pgsql')
 
   it('works', function()
     local input_object = {
       tags = {
         highway = 'service',
-        serivce = "alley",
-        mapillary = "123",
+        serivce = 'alley',
+        mapillary = '123',
       },
       id = 1,
       type = 'way',
     }
     local result = result_tags_roads(input_object)
-    assert.are.equal(result.id, "way/"..input_object.id)
-    assert.are.equal(type(result.meta.updated_at), 'number')
-    assert.are.equal(result.tags.highway, input_object.tags.highway)
-    assert.are.equal(result.tags.osm_mapillary, input_object.tags.mapillary)
+    assert.are.equal('way/'..input_object.id, result.id)
+    assert.are.equal('number', type(result.meta.updated_at))
+    assert.are.equal(input_object.tags.highway, result.tags.highway)
+    assert.are.equal(input_object.tags.mapillary, result.tags.osm_mapillary)
   end)
 
   it('width > offset', function()
     local input_object = {
       tags = {
         highway = 'service',
-        serivce = "alley",
-        width = "80",
+        serivce = 'alley',
+        width = '80',
       },
       id = 1,
       type = 'way',
     }
     local result = result_tags_roads(input_object)
-    assert.are.equal(result.id, "way/"..input_object.id)
-    assert.are.equal(type(result.meta.updated_at), 'number')
-    assert.are.equal(result.tags.width, 80)
-    assert.are.equal(result.tags.offset_left, 40.0)
-    assert.are.equal(result.tags.offset_right, 40.0)
+    assert.are.equal('way/'..input_object.id, result.id)
+    assert.are.equal('number', type(result.meta.updated_at))
+    assert.are.equal(80, result.tags.width)
+    assert.are.equal(40.0, result.tags.offset_left)
+    assert.are.equal(40.0, result.tags.offset_right)
   end)
 end)
