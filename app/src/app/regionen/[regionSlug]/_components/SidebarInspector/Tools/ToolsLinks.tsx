c3b9ce1dbd98c7be00c53db9c3b3299b105f6f39
Tobias <tobias@fixmycity.de>
2025-07-16 21:44:46 +0200

    Inspector: Refactor changeset links and add osmcha, osm
    We now have the changeset_id to do this

--- app/src/app/regionen/[regionSlug]/_components/SidebarInspector/Tools/ToolsLinks.tsx
+++ app/src/app/regionen/[regionSlug]/_components/SidebarInspector/Tools/ToolsLinks.tsx
@@ -29,8 +29,26 @@ export const ToolsLinks = ({ feature, editors, osmIdConfig }: Props) => {
   const osmUrlHref = osmOrgUrl(osmTypeId)
   const osmEditIdUrlHref = osmEditIdUrl(osmTypeId)
   const osmEditRapidUrlHref = osmEditRapidUrl(osmTypeId)
-  const historyUrlHref = historyUrl(osmTypeId)
   const mapillaryUrlHref = mapillaryUrl(feature.geometry)
+
+  const changesetLinks = [
+    {
+      title: 'Änderungshistorie',
+      url: historyUrl(osmTypeId),
+    },
+    {
+      title: 'Changeset',
+      url:
+        Boolean(feature.properties.changeset_id) &&
+        `https://www.openstreetmap.org/changeset/${feature.properties.changeset_id}`,
+    },
+    {
+      title: 'OSMCha',
+      url:
+        Boolean(feature.properties.changeset_id) &&
+        `https://osmcha.org/changesets/${feature.properties.changeset_id}`,
+    },
+  ]
 
   return (
     <section className="flex flex-wrap gap-3 pb-1 text-xs">
@@ -68,12 +86,6 @@ export const ToolsLinks = ({ feature, editors, osmIdConfig }: Props) => {
         </LinkExternal>
       )}
 
-      {historyUrlHref && (
-        <LinkExternal blank button href={historyUrlHref}>
-          Änderungshistorie
-        </LinkExternal>
-      )}
-
       {mapillaryUrlHref && (
         <LinkExternal blank button href={mapillaryUrlHref}>
           Mapillary
@@ -90,6 +102,20 @@ export const ToolsLinks = ({ feature, editors, osmIdConfig }: Props) => {
         geometry={feature.geometry}
         osmIdConfig={osmIdConfig}
       />
+
+      <div>
+        {changesetLinks.map(({ title, url }) => {
+          if (!url) return null
+          return (
+            <>
+              <LinkExternal key={url} blank href={url}>
+                {title}
+              </LinkExternal>
+              <span className="last:hidden"> &bull; </span>
+            </>
+          )
+        })}
+      </div>
     </section>
   )
 }
