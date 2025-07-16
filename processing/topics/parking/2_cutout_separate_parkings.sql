5c72955e71505ecd10513e5d2a0c6ebf69bb3fc7
Laurenz Rasche <laurenz@fixmycity.de>
2025-07-16 15:56:59 +0200

    estimate lines from separate parking points

--- processing/topics/parking/2_cutout_separate_parkings.sql
+++ processing/topics/parking/2_cutout_separate_parkings.sql
@@ -45,3 +45,49 @@ FROM
       p.geom
     )
   ) AS d;
+
+-- INFO: Drop table happesn in cutout_parkings.sql
+--
+-- PROCESS
+INSERT INTO
+  _parking_parkings_cutted (
+    id,
+    osm_type,
+    osm_id,
+    tags,
+    side,
+    source,
+    meta,
+    geom
+  )
+SELECT
+  COALESCE(
+    p.kerb_id || '/' || p.id || '/' || d.path[1],
+    p.kerb_id || '/' || p.id
+  ),
+  p.osm_type,
+  osm_id,
+  p.tags,
+  p.kerb_side,
+  'separate_parking_points' as source,
+  p.meta,
+  d.geom
+FROM
+  _parking_separate_parking_points_projected p,
+  LATERAL ST_Dump (
+    COALESCE(
+      ST_Difference (
+        p.geom,
+        (
+          SELECT
+            ST_Union (c.geom)
+          FROM
+            _parking_cutouts c
+          WHERE
+            c.geom && p.geom
+            AND c.tags ->> 'source' <> 'separate_parking_points'
+        )
+      ),
+      p.geom
+    )
+  ) AS d;
