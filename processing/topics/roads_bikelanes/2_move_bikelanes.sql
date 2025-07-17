70acd1674531730e8c7d07e0123b53ddeff525fb
Tobias <tobias@fixmycity.de>
2025-07-17 09:27:57 +0200

    Processgin: Fix orientation for single lane bikelanes
    Follow up to b9157ec6ca20799ca008755ecbf9ab271482106f

--- processing/topics/roads_bikelanes/2_move_bikelanes.sql
+++ processing/topics/roads_bikelanes/2_move_bikelanes.sql
@@ -5,7 +5,7 @@
 --      - Good, because we now have a different direction per side
 --      - Bad, because the direction is for left hand traffic, now (See step 3)
 --    - Additionally we check wether the geometry is `simple` because otherwise we might get a MLString for the same reason we simplify the geometries
--- 3. Reverse direction of geometry for all ways (see "Bad" above)
+-- 3. Reverse direction of geometry for all ways that we touched before (see "Bad" above)
 --
 DO $$ BEGIN RAISE NOTICE 'START move bikelanes by offset %', clock_timestamp(); END $$;
 
@@ -25,4 +25,8 @@ WHERE
 
 UPDATE bikelanes
 SET
-  geom = ST_Reverse (geom);
+  geom = ST_Reverse (geom)
+WHERE
+  ST_IsSimple (geom)
+  AND NOT ST_IsClosed (geom)
+  AND tags ? 'offset';
