6c4d93eb990fd1cbc0342e44069e0a5bf8773f6c
Tobias <tobias@fixmycity.de>
2025-07-29 21:08:02 +0200

    Inspector: Introduce secondary rows by prefix string "tilda_"

--- app/src/app/regionen/[regionSlug]/_components/SidebarInspector/TagsTable/TagsTableRow.tsx
+++ app/src/app/regionen/[regionSlug]/_components/SidebarInspector/TagsTable/TagsTableRow.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { twJoin } from 'tailwind-merge'
 import { SourcesId } from '../../../_mapData/mapDataSources/sources.const'
 import { TagsTableRowValueWithTooltip } from './TagsTableRowValueWithTooltip'
 import { NodataFallback } from './compositTableRows/NodataFallback'
@@ -20,12 +21,25 @@ export type TagsTableRowProps =
     }
 
 export const TagsTableRow = ({ sourceId, tagKey, tagValue, children }: TagsTableRowProps) => {
+  const secondaryRowPrefixes = ['tilda_']
+  const isSecodaryRow = secondaryRowPrefixes.some((e) => tagKey.startsWith(e))
+
   return (
     <tr className="group">
-      <td className="w-2/5 py-2 pl-4 pr-3 text-sm font-medium text-gray-900">
+      <td
+        className={twJoin(
+          'w-2/5 py-2 pl-4 pr-3 text-sm font-medium',
+          isSecodaryRow ? 'text-gray-400 group-hover:text-gray-900' : 'text-gray-900',
+        )}
+      >
         <ConditionalFormattedKey sourceId={sourceId} tagKey={tagKey} />
       </td>
-      <td className="px-3 py-2 text-sm text-gray-500">
+      <td
+        className={twJoin(
+          'px-3 py-2 text-sm',
+          isSecodaryRow ? 'text-gray-400 group-hover:text-gray-500' : 'text-gray-500',
+        )}
+      >
         {tagValue === null && <NodataFallback />}
         {tagValue === undefined && !children && <NodataFallback />}
         {tagValue && (
