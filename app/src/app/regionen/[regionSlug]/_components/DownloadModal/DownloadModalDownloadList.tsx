c6baa3dd6ca42011e709aeb8d93994d28a839f07
Tobias <tobias@fixmycity.de>
2025-08-18 20:07:11 +0200

    Export: Improve UI
    Follow up to 838d19f4324c4401dd251a7d651086cb5d3fdf77

--- app/src/app/regionen/[regionSlug]/_components/DownloadModal/DownloadModalDownloadList.tsx
+++ app/src/app/regionen/[regionSlug]/_components/DownloadModal/DownloadModalDownloadList.tsx
@@ -23,7 +23,7 @@ export const DownloadModalDownloadList = () => {
         if (!sourceData.export.apiIdentifier) return null
 
         return (
-          <li key={sourceData.id} className="py-5">
+          <li key={sourceData.id} className="pb-4 pt-5">
             <h3 className="mb-1 text-sm font-bold text-purple-800">{sourceData.export.title}:</h3>
 
             <table className="my-2 text-sm text-gray-500">
@@ -62,35 +62,42 @@ export const DownloadModalDownloadList = () => {
                       bbox,
                       param as Formats,
                     )}
-                    className="text-xs"
+                    classNameOverwrite="w-28 flex-none rounded-md border border-gray-300 bg-gray-50 px-3 py-2 shadow-sm hover:bg-yellow-50 focus:ring-1 focus:ring-yellow-500"
                     download
                     blank
                   >
-                    {name}
+                    <strong className="mb-0.5 block text-xs font-medium text-gray-500">
+                      Download
+                    </strong>
+                    <span className="block w-full border-0 p-0 font-mono text-gray-900 focus:ring-0 sm:text-sm">
+                      {name}
+                    </span>
                   </LinkExternal>
                 )
               })}
             </div>
-            <div className="mt-2">
-              <div className="rounded-md border border-gray-200 px-3 py-2 shadow-sm">
-                <label
-                  htmlFor={sourceData.id}
-                  className="mb-0.5 block text-[10px] font-medium text-gray-700"
-                >
-                  Vector Tile URL
-                </label>
-                <input
-                  type="text"
-                  name={sourceData.id}
-                  id={sourceData.id}
-                  className="block w-full border-0 p-0 font-mono text-xs text-gray-500 placeholder-gray-400 focus:ring-0"
-                  placeholder="Vector Tile URL"
-                  defaultValue={sourceData.tiles}
-                  onFocus={(event) => event.target.select()}
-                  readOnly
-                />
+            <details className="group mt-2">
+              <summary className="cursor-pointer text-xs text-gray-500 hover:underline group-hover:text-gray-800">
+                Vector Tile URL
+              </summary>
+              <div className="mt-2">
+                <div className="rounded-md border border-gray-200 px-3 py-2 shadow-sm">
+                  <label htmlFor={sourceData.id} className="sr-only">
+                    Vector Tile URL
+                  </label>
+                  <input
+                    type="text"
+                    name={sourceData.id}
+                    id={sourceData.id}
+                    className="block w-full border-0 p-0 font-mono text-xs text-gray-500 placeholder-gray-400 focus:ring-0"
+                    placeholder="Vector Tile URL"
+                    defaultValue={sourceData.tiles}
+                    onFocus={(event) => event.target.select()}
+                    readOnly
+                  />
+                </div>
               </div>
-            </div>
+            </details>
           </li>
         )
       })}
