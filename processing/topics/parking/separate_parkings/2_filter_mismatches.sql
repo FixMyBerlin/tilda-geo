d32d0206bd56396f4cc07f6697c2fed163b1b8b8
Laurenz Rasche <laurenz@fixmycity.de>
2025-08-19 13:46:33 +0200

    filter mismatches with kerb_id

--- processing/topics/parking/separate_parkings/2_filter_mismatches.sql
+++ processing/topics/parking/separate_parkings/2_filter_mismatches.sql
@@ -1,31 +1,28 @@
 DO $$ BEGIN RAISE NOTICE 'START filtering separate parking where the kerb has `parking != separate`%', clock_timestamp(); END $$;
 
 SELECT
-  osm_id,
-  side INTO TEMP separate_parkings
+  id INTO TEMP separate_parkings
 FROM
   _parking_road_parkings
 WHERE
   tags ->> 'parking' = 'separate';
 
-CREATE INDEX separate_parkings_idx ON separate_parkings (osm_id, side);
+CREATE INDEX separate_parkings_idx ON separate_parkings (id);
 
 DELETE FROM _parking_separate_parking_areas_projected
 WHERE
-  (kerb_osm_id, kerb_side) NOT IN (
+  kerb_id NOT IN (
     SELECT
-      osm_id,
-      side
+      id
     FROM
       separate_parkings
   );
 
 DELETE FROM _parking_separate_parking_points_projected
 WHERE
-  (kerb_osm_id, kerb_side) NOT IN (
+  kerb_id NOT IN (
     SELECT
-      osm_id,
-      side
+      id
     FROM
       separate_parkings
   );
