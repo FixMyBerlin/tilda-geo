f301aa82604eac7ba243d0d14e5a385ed631e964
Laurenz Rasche <laurenz@fixmycity.de>
2025-09-15 15:16:29 +0200

    exclude separate parking cutouts from final cutout table

--- processing/topics/parking/7_finalize_parkings.sql
+++ processing/topics/parking/7_finalize_parkings.sql
@@ -47,7 +47,11 @@ SELECT
 FROM
   _parking_cutouts pc
 WHERE
-  tags ->> 'source' <> 'parking_roads';
+  tags ->> 'source' NOT IN (
+    'parking_roads',
+    'separate_parking_areas',
+    'separate_parking_points'
+  );
 
 -- explode parkings into quantized points
 DROP TABLE IF EXISTS parkings_quantized;
