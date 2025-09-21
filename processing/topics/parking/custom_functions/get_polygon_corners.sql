198c3abf69d5b06f9ea69ba8652c2ac027262dac
Laurenz Rasche <laurenz@fixmycity.de>
2025-09-21 22:43:51 +0200

    parking: parametrize get_polygon_corners with `n_corners` instead of `max_angle_degrees`

--- processing/topics/parking/custom_functions/get_polygon_corners.sql
+++ processing/topics/parking/custom_functions/get_polygon_corners.sql
@@ -1,48 +1,52 @@
 DROP FUNCTION IF EXISTS get_polygon_corners;
 
-CREATE FUNCTION get_polygon_corners (poly geometry, max_angle_degrees float) RETURNS TABLE (
-  corner_idx integer,
+DROP FUNCTION IF EXISTS get_angle_on_line;
+
+CREATE FUNCTION get_angle_on_line (geom geometry, idx integer) RETURNS double precision AS $$
+DECLARE
+  n INT := ST_NumPoints(geom);
+  a geometry;
+  b geometry;
+  c geometry;
+  angle double precision;
+BEGIN
+  a := ST_PointN(geom, CASE WHEN idx > 1 THEN idx - 1 ELSE n - 1 END);
+  b := ST_PointN(geom, idx);
+  c := ST_PointN(geom, idx % n + 1);
+
+  angle := ST_Angle(a, b, c);
+  IF angle > pi() THEN
+    angle := 2 * pi() - angle;
+  END IF;
+
+  RETURN angle;
+END;
+$$ LANGUAGE plpgsql IMMUTABLE;
+
+CREATE FUNCTION get_polygon_corners (poly geometry, n_corners integer) RETURNS TABLE (
+  corner_idx BIGINT,
   geom geometry,
   angle double precision
 ) AS $$
 DECLARE
   ring geometry := ST_ExteriorRing(ST_ForceRHR(poly));
   n INT := ST_NumPoints(ring);
-  i INT;
-  effective_angle double precision;
-  a geometry;
-  b geometry;
-  c geometry;
 BEGIN
-  -- If the polygon is not a valid polygon, return nothing
   IF n IS NULL THEN
     RETURN;
   END IF;
-
-  corner_idx := 1;
-  FOR i IN 1..n LOOP
-    -- 1 and n are the same point so we have to skip them at the boundary cases
-    a := ST_PointN(ring, CASE WHEN i > 1 THEN i - 1 ELSE n - 1 END);
-    b := ST_PointN(ring, i);
-    c := ST_PointN(ring, CASE WHEN i < n THEN i + 1 ELSE 2 END);
-
-    angle := ST_Angle(a, b, c);
-
-
-    IF angle < pi() THEN
-      effective_angle := degrees(angle);
-    ELSE
-      effective_angle := degrees(2 * pi() - angle);
-    END IF;
-
-    angle := degrees(angle);
-
-    IF effective_angle < max_angle_degrees THEN
-      geom := b;
-      RETURN NEXT;
-      corner_idx := corner_idx + 1;
-    END IF;
-  END LOOP;
+  -- Single query to get the sharpest corners using get_angle_on_line, no loop
+  RETURN QUERY
+  WITH corners AS (
+    SELECT
+      ST_PointN(ring, idx) AS geom,
+      get_angle_on_line(ring, idx) AS angle,
+      idx
+    FROM generate_series(1, n) AS idx
+    ORDER BY get_angle_on_line(ring, idx) ASC
+    LIMIT n_corners )
+  SELECT ROW_NUMBER() OVER (ORDER BY c.idx) AS corner_idx, c.geom, c.angle
+  FROM corners c;
 
   RETURN;
 END;
