19b0cfe3e5ec1e5d4cfd4914a3d6482c0091e663
Tobias <tobias@fixmycity.de>
2025-09-24 17:46:21 +0200

    Gzip: Use native Node 22 node:zlip instead of spanSync gzip
    This is supposed to be about 2.3x faster

--- app/scripts/StaticDatasets/updateStaticDatasets/getDecompressedFilename.ts
+++ app/scripts/StaticDatasets/updateStaticDatasets/getDecompressedFilename.ts
@@ -1,27 +1,24 @@
 import fs from 'node:fs'
 import path from 'node:path'
+import { gunzipSync } from 'node:zlib'
 import { red } from '../utils/log'
 
 /** @description Check the file. If compressed, copy it, uncompress it, return that filename. */
 export const getDecompressedFilename = ({ inputFilename, outputFilename, outputFolder }) => {
   if (path.parse(inputFilename).ext === '.gz') {
-    // When our input is zipped, we copy the file to the temp folder and unzip it, which replaces the given file
+    // When our input is zipped, we decompress it directly using native zlib
     console.log(`  Unzipping file...`)
-    const compressedCopy = path.join(outputFolder, `${outputFilename}.decompressed.geojson.gz`)
-    // Using Bun.spanSync to copy the file did not work; using Bun to copy did not work either.
-    fs.copyFileSync(inputFilename, compressedCopy)
+    const decompressedFilename = path.join(outputFolder, `${outputFilename}.decompressed.geojson`)
 
-    // TODO: Maybe use pako.ungzip() here instead, we use pako in `StaticDatasets/updateStaticDatasets/isCompressedSmallerThan.ts`
-    const bunFeedback = Bun.spawnSync(['gzip', '--decompress', '--force', compressedCopy])
-    if (bunFeedback.success === false) {
-      red(
-        'ERROR with Bun.spawnSync gzip decompress',
-        bunFeedback.stdout.toString(),
-        // @ts-expect-error this came up after updating packages; don't see a clear way to reasolve this nor something in the docs that we need to change…
-        bunFeedback.stderr.toString(),
-      )
+    try {
+      const compressedData = fs.readFileSync(inputFilename)
+      const decompressedData = gunzipSync(compressedData)
+      fs.writeFileSync(decompressedFilename, decompressedData)
+      return decompressedFilename
+    } catch (error) {
+      red('ERROR with native zlib gunzip decompression:', error.message)
+      throw error
     }
-    return compressedCopy.replace('.gz', '')
   }
   return inputFilename
 }
