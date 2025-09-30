a2c2158c2a97555a5e8df2226dc917865611f563
Tobias <tobias@fixmycity.de>
2025-09-30 13:42:07 +0200

    Bikelanes: Fix new process logic for shared categories
    Follow up to 35fdf4e62a44d2baf1b40a2f6518034e3a4be774

--- processing/topics/roads_bikelanes/bikelanes/categories/CreateSubcategoriesAdjoiningOrIsolated.lua
+++ processing/topics/roads_bikelanes/bikelanes/categories/CreateSubcategoriesAdjoiningOrIsolated.lua
@@ -9,7 +9,8 @@ function CreateSubcategoriesAdjoiningOrIsolated(category)
     implicitOneWayConfidence = category.implicitOneWayConfidence,
     condition = function(tags)
       return category(tags) and IsSidepath(tags) and tags.is_sidepath ~= "no"
-    end
+    end,
+    process = category.process,
   })
   local isolated = BikelaneCategory.new({
     id = category.id .. '_isolated',
@@ -20,7 +21,8 @@ function CreateSubcategoriesAdjoiningOrIsolated(category)
     condition = function(tags)
       -- `hw=service` handles `footAndCyclewayShared` on emergency access ways or driveways
       return category(tags) and (tags.is_sidepath == "no" or tags.highway == 'service')
-    end
+    end,
+    process = category.process,
   })
   local adjoiningOrIsolated = BikelaneCategory.new({
     id = category.id .. '_adjoiningOrIsolated',
@@ -32,7 +34,8 @@ function CreateSubcategoriesAdjoiningOrIsolated(category)
       -- Trigger on every value other than yes or no (not is_sidepath == yes and not is_sidepath == no)
       -- Also exclude highway=service as it's treated as isolated
       return category(tags) and not IsSidepath(tags) and tags.is_sidepath ~= "no" and tags.highway ~= 'service'
-    end
+    end,
+    process = category.process,
   })
   return adjoining, isolated, adjoiningOrIsolated
 end
