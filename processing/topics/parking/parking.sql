390f069d6fb5f4fac43b20287f5388335c27d494
Tobias <tobias@fixmycity.de>
2025-10-16 14:25:41 +0200

    Parking: Reenable parking qa
    Disabled by accident? in a3b264dcf2a29eddd25e7b1ab3a896aa9d38fce4

--- processing/topics/parking/parking.sql
+++ processing/topics/parking/parking.sql
@@ -78,9 +78,7 @@
 \i '/processing/topics/parking/5_estimate_parking_capacities.sql'
 \i '/processing/topics/parking/6_filter_parkings.sql'
 \i '/processing/topics/parking/7_finalize_parkings.sql'
--- \i '/processing/topics/parking/8_qa_parkings_euvm_voronoi.sql'
+\i '/processing/topics/parking/8_qa_parkings_euvm_voronoi.sql'
 \i '/processing/topics/parking/9_create_labels.sql'
-
-
 
 DO $$ BEGIN RAISE NOTICE 'FINISH topics/parking/parking.sql at %', clock_timestamp() AT TIME ZONE 'Europe/Berlin'; END $$;
