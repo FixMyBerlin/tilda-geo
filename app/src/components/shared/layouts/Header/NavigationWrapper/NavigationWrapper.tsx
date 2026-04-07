71ecff99f7705095a224ff5f880970275e097298
Tobias (FMC) <tobias@fixmycity.de>
2026-04-07 12:28:35 +0200

    Map: Fix primary navigation width

--- app/src/components/shared/layouts/Header/NavigationWrapper/NavigationWrapper.tsx
+++ app/src/components/shared/layouts/Header/NavigationWrapper/NavigationWrapper.tsx
@@ -1,9 +1,5 @@
 type Props = { children: React.ReactNode }
 
 export const NavigationWrapper = ({ children }: Props) => {
-  return (
-    <nav className="z-10 bg-gray-800 shadow-xl">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">{children}</div>
-    </nav>
-  )
+  return <nav className="z-10 w-full bg-gray-800 px-4 shadow-xl sm:px-6 lg:px-8">{children}</nav>
 }
