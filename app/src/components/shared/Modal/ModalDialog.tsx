eee8741953178974cd66579c5b8b00b8dd616419
Tobias (FMC) <tobias@fixmycity.de>
2026-04-10 10:19:45 +0200

    Layout: Align modal width with content pages

--- app/src/components/shared/Modal/ModalDialog.tsx
+++ app/src/components/shared/Modal/ModalDialog.tsx
@@ -57,7 +57,7 @@ export const ModalDialog = ({ title, icon, open, setOpen, buttonCloseName, child
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
           <DialogPanel
             transition
-            className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-3 pb-4 text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-lg sm:px-6 sm:pt-4 sm:pb-6 data-closed:sm:translate-y-0 data-closed:sm:scale-95"
+            className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-3 pb-4 text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-prose sm:px-6 sm:pt-4 sm:pb-6 data-closed:sm:translate-y-0 data-closed:sm:scale-95"
           >
             <div className="flex items-center gap-3">
               <div
