f8117c35e554ff29d72ef5d863805366d0fcbc41
Tobias <tobias@fixmycity.de>
2026-06-22 21:31:19 +0200

    Inspector: Fix missing translations for mapillary

--- app/src/components/regionen/pageRegionSlug/SidebarInspector/TagsTable/translations/translations.const.ts
+++ app/src/components/regionen/pageRegionSlug/SidebarInspector/TagsTable/translations/translations.const.ts
@@ -1,7 +1,9 @@
 import generatedTopicDocsTranslations from '@/data/generated/topicDocs/inspectorTranslations.gen'
 import { translationsParkingLars } from './translationsParkingLars.const'
+import { translationsSources } from './translationsSources.const'
 
 export const translations: { [key: string]: string } = {
   ...translationsParkingLars,
+  ...translationsSources,
   ...(generatedTopicDocsTranslations as Record<string, string>),
 }
